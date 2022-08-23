fd251c555c3eaf2e816246a15d304d911a1eebfc
Jonathas <jonathass5678@gmail.com>
2022-08-23 14:31:55 -0300

    casadeadoracao

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fileUpload = require('express-fileupload');
+const axios = require('axios');
+
+const app = express();
+
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : path.join(__dirname, 'temp')
+}));
+require('dotenv').config();
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/pages'));
+
+app.get('/',async (req, res) => {
+    await axios.get(process.env.URL_API_INICIO_SOBRE).then(function(data){
+        var iniciosobre = data.data.map(function(val){
+            return {
+                inicio: val.inicio,
+                sobre: val.sobre,
+                contato: val.contato
+            }
+        })
+        axios.get(process.env.URL_API_PROJETO).then(function(data){
+            var projetos = data.data.map(function(val){
+                return {
+                    img: val.img,
+                    title: val.title,
+                    sobre: val.sobre
+                }
+            })
+            axios.get(process.env.URL_API_CONTECONOSCO).then(function(data){
+                var conteconosco = data.data.map(function(val){
+                    return {
+                        img_url: val.img_url,
+                        nome: val.nome,
+                        type: val.type
+                    }
+                })
+                axios.get(process.env.URL_API_LINKS_CONTATO).then(function(data){
+                    var linkscontato = data.data.map(function(val){
+                        return {
+                            img: val.img,
+                            title: val.title,
+                            url: val.url
+                        }
+                    })
+                res.render('home',{data_iniciosobre:iniciosobre,dataprojetos:projetos,data_cont:conteconosco,contat:linkscontato});
+            });
+        });
+    })
+})
+})
+app.get('/escoladefundamentos',async (req, res) => {
+    await axios.get(process.env.URL_API_INICIO_SOBRE).then(function(data){
+        var iniciosobre = data.data.map(function(val){
+            return {
+                inicio: val.inicio,
+                sobre: val.sobre,
+                contato: val.contato
+            }
+        })
+        axios.get(process.env.URL_API_LINKS_CONTATO).then(function(data){
+            var linkscontato = data.data.map(function(val){
+                return {
+                    img: val.img,
+                    title: val.title,
+                    url: val.url
+                }
+            })
+        axios.get(process.env.URL_API_ESCOLADEFUNDAMENTOS).then(function(data){
+            var edfcontent = data.data.map(function(val){
+                return {
+                    Visao: val.Visao,
+                    Missao: val.Missao,
+                    Desenvolvimento: val.Desenvolvimento,
+                    Modulos: val.Modulos,
+                    Mestres: val.Mestres,
+                    link_curso: val.link_curso
+                }
+            })
+                res.render('edf',{data_iniciosobre:iniciosobre,contat:linkscontato,edf:edfcontent});
+            })
+        })
+    })
+})
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log('server rodando');
+})
---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+window.addEventListener('scroll', function() {
+    const parallax = document.querySelector('#texts');
+    let scrollPosition = window.pageYOffset;
+    parallax.style.transform = 'translateY(' + scrollPosition * 0.37 + 'px)';
+});
+
+var typed = new Typed(".typing", {
+    strings: ["Palavras", "Louvor", "Dança", "Teatro"],
+    typeSpeed: 60,
+    backSpeed: 50,
+    loop: true
+});
+var typed = new Typed(".typing-2", {
+    strings: ["Adoração", "Comunhão", "Discipulado", "Serviço", "Evangelismo"],
+    typeSpeed: 60,
+    backSpeed: 50,
+    loop: true
+});
+
+const openMenu = document.querySelector('.mobile');
+const btnOpen = document.querySelector('.open');
+const btnClose = document.querySelector('.close');
+
+btnOpen.addEventListener('click', function() {
+    openMenu.style.display = 'block';
+})
+btnClose.addEventListener('click', function() {
+    openMenu.style.display = 'none';
+})
